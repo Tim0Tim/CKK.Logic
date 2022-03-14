c244d06eac583febddb6edce05bae70f96033d52
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-03-14 09:55:38 -0600

    test change

--- CKK.Logic/Models/ShoppingCartItem.cs
+++ CKK.Logic/Models/ShoppingCartItem.cs
@@ -37,8 +37,8 @@ namespace CKK.Logic.Models
             _product = product;
         }
 
-        public decimal GetTotal(decimal totalPrice)
-        {
+        static public decimal GetTotal(decimal totalPrice)
+        { 
             return totalPrice;
         }
     }
