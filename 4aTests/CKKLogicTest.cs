8aa220ef850899a2009a3a9568f67aad2dac54a4
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-04-12 10:59:47 -0600

    created test skeleton

--- 4aTests/CKKLogicTest.cs
+++ 4aTests/CKKLogicTest.cs
@@ -1,12 +1,44 @@
 using System;
 using Xunit;
 
+
+
 namespace _4aTests
 {
     public class CKKLogicTest
     {
         [Fact]
-        public void GetIdPass()
+        public void GetTotalPass()
+        {
+
+        }
+
+        [Fact]
+        public void GetTotalFail()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveProductPass()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveProductFail()
+        {
+
+        }
+
+        [Fact]
+        public void AddProductPass()
+        {
+
+        }
+
+        [Fact]
+        public void AddProductFail()
         {
 
         }
