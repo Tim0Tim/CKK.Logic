6cf67a319b3e3881da68262376ebcc15c44a188a
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-06-20 10:57:02 -0600

    test change in shopping cart

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -89,8 +89,9 @@ namespace CKK.Logic.Models
             {
                 if (element.GetQuantity() <= 0 && element.GetProduct().GetId() == id)
                 {
-                    element.SetQuantity(0);
-                    return element;
+                    Products.Remove(element);
+                    //element.SetQuantity(0);
+                    //return element;
                 }
                 else if( element.GetProduct().GetId() == id)
                 {
